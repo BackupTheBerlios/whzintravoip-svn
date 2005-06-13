9a6a3925c98d464a3e9467506c0dad50e9359865
holger2005 <holger2005@7f7f3491-28f8-0310-9b85-f925ea95e006>
2005-06-13 18:35:27 +0000

    Now working, with delay too (in millis). cheers and have a nice...

--- src/de/fh_zwickau/pti/whzintravoip/thin_client/PlayTunes.java
+++ src/de/fh_zwickau/pti/whzintravoip/thin_client/PlayTunes.java
@@ -1,6 +1,7 @@
 package de.fh_zwickau.pti.whzintravoip.thin_client;
 
 import javax.media.*;
+import java.util.*;
 
 /**
  * <p>Überschrift: WHZintraVoIP</p>
@@ -19,40 +20,71 @@ public class PlayTunes {
     private Player m_Player = null;
     private boolean m_bDebug = true;
     private ThinClientGUI userGUI = null;
+    private HashMap m_PlayerMap = new HashMap();
+    private String m_sSoundKey = null;
+    private int m_iDelay = 0;
 
     // the Sounds
-//    private String m_sRing = "file:///s1.wav";
-    private String m_sRing = "file://D:/Coding/Java/WHZIntraVoIP/s1.wav";
+    private String m_sFile = null;
 
     // Now work on
     public PlayTunes() {
-        initRing();
-        playRing();
+        // just for test, may be deleted if no longer need
+        /*initTune("file:///s2.wav", "ring", 2000);
+        playTune("ring");*/
     }
 
+    /**
+     * Instantiate a new PlayTunes within a Message instance.
+     * @param userGUI ThinClientGUI the instance
+     */
     public PlayTunes(ThinClientGUI userGUI) {
         this.userGUI = userGUI;
-        initRing();
     }
 
-    public static void main(String[] args) {
+    // just for test, may be deleted if no longer need
+    /*public static void main(String[] args) {
         new PlayTunes();
-    }
-
-
-    /**
-     * Play the ringing.
-     */
-    public void playRing() {
-        m_Player.start();
-    }
+    }*/
 
     /**
-     * Stop Ringing on ackknowledge call before sounds finished.
+     * Start Playing.
+     * @param key String Player key
      */
-    public void stopRing() {
-        m_Player.stop();
-        close_Player();
+    public void playTune(String key) {
+        this.m_sSoundKey = key;
+        if (m_PlayerMap.containsKey(m_sSoundKey)) {
+            this.m_Player = (Player) m_PlayerMap.get(m_sSoundKey);
+        } else {
+            m_Player = null;
+        }
+        if (m_Player != null) {
+            this.m_Player = (Player) m_PlayerMap.get(m_sSoundKey);
+            m_Player.start();
+        } else {
+            errMsg("Error during playTune -> no Player found on key: " +
+                   m_sSoundKey);
+        }
+    }
+
+    /**
+     * Stop playing.
+     * @param key String Player key
+     */
+    public void stopTune(String key) {
+        this.m_sSoundKey = key;
+        if (m_PlayerMap.containsKey(m_sSoundKey)) {
+            this.m_Player = (Player) m_PlayerMap.get(m_sSoundKey);
+        } else {
+            m_Player = null;
+        }
+        if (m_Player != null) {
+            this.m_Player = (Player) m_PlayerMap.get(m_sSoundKey);
+            m_Player.stop();
+        } else {
+            errMsg("Error during stopTune -> no Player found on key: " +
+                   m_sSoundKey);
+        }
     }
 
     /**
@@ -62,7 +94,7 @@ public class PlayTunes {
     private void errMsg(String msg) {
         if (m_bDebug) {
             System.out.println(msg);
-        }else{
+        } else {
             userGUI.errOutput(msg);
         }
     }
@@ -74,34 +106,64 @@ public class PlayTunes {
     private void infMsg(String msg) {
         if (m_bDebug) {
             System.out.println(msg);
-        }else{
+        } else {
             userGUI.stdOutput(msg);
         }
     }
 
     /**
      * Create a Player over JMF Api to play some ringing tune.
+     * @param file String The Filename in Java norm
+     * @param key String The Player Key
+     * @param delay int Delay for playing endles than one times in millis
+     * @throws Exception
      */
-    private void initRing() {
+    public void initTune(String file, String key, int delay) {
+        this.m_sFile = file;
+        this.m_sSoundKey = key;
+        this.m_iDelay = delay;
         try {
-            m_Player = Manager.createRealizedPlayer(new MediaLocator(m_sRing));
+            m_Player = Manager.createRealizedPlayer(new MediaLocator(m_sFile));
+            Time ti = m_Player.getMediaTime();
             m_Player.addControllerListener(new ControllerAdapter() {
                 public void endOfMedia(EndOfMediaEvent e) {
-                    m_Player.stop();
-                    close_Player();
+                    boolean delay = true;
+                    if (m_iDelay != 0) {
+                        long startTime = System.currentTimeMillis();
+                        m_Player.stop();
+                        Time time = new Time(0);
+                        m_Player.setMediaTime(time);
+                        while (true) {
+                            if ((System.currentTimeMillis() - startTime) >
+                                m_iDelay) {
+                                m_Player.start();
+                                break;
+                            }
+                        }
+                    } else {
+                        m_Player.stop();
+                    }
                 }
             });
-
         } catch (Exception ex) {
-            errMsg("Error during playRing: " + ex.getMessage());
+            errMsg("Error on initTune: " + ex.toString());
         }
-        infMsg("Player init!");
+        if (m_Player != null) {
+            m_PlayerMap.put(m_sSoundKey, m_Player);
+        }
     }
 
     /**
      * Close the Player when finished.
+     * @param key String Player Key
      */
-    private void close_Player() {
+    public void close_Player(String key) {
+        this.m_sSoundKey = key;
+        if (m_PlayerMap.containsKey(m_sSoundKey)) {
+            this.m_Player = (Player) m_PlayerMap.get(m_sSoundKey);
+        } else {
+            m_Player = null;
+        }
         if (m_Player != null) {
             try {
                 m_Player.deallocate();
@@ -111,7 +173,7 @@ public class PlayTunes {
                 errMsg("Error while closing Player: " + ex.getMessage());
             }
         } else {
-            errMsg("No Player initialized yet!");
+            errMsg("No Player found for key: " + m_sSoundKey);
         }
     }
 }
